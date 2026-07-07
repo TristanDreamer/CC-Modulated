476b9e4d1bbc16dcbd3004e042f33fda42403d48
BlackStar8107 <123768601+BlackStar8107@users.noreply.github.com>
2026-07-07 01:02:56 +0100

    Removed left-over test

--- webservice/src/main.py
+++ webservice/src/main.py
@@ -8,9 +8,3 @@ from. import events, routes
 
 app.register_blueprint(events.bp)
 app.register_blueprint(routes.bp)
-
-@app.route("/test")
-def test():
-    from flask import render_template
-    return render_template("root.html")
-    return "<p1>Test</p1>"
