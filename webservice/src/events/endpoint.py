37ff57049ec374236c19f31c02cfa44f4b02a4be
BlackStar8107 <123768601+BlackStar8107@users.noreply.github.com>
2026-07-12 21:00:38 +0100

    created sample endpoint

--- webservice/src/events/endpoint.py
+++ webservice/src/events/endpoint.py
@@ -1,6 +1,7 @@
 import json
 from ..main import sock
 from .endpoint_command_list import COMMAND_LIST
+from typing import Tuple
 
 @sock.route('/endpoint')
 def endpoint_main(ws):
@@ -13,10 +14,17 @@ def endpoint_main(ws):
         packet = ws.receive()
 
 
-def decode_packet(packet) -> tuple[int, str]:
+def decode_packet(packet) -> Tuple[int, str]:
     try:
-        json_pack = json.dumps(packet)
+        json_pack = json.loads(packet)
 
+        command = json_pack["command"]
+        data = json_pack["data"]
+
+        if command in COMMAND_LIST:
+            return COMMAND_LIST[command]
+        else:
+            return(-2, "Unknown Command")
 
     except Exception as E:
         return (-1, str(E))
